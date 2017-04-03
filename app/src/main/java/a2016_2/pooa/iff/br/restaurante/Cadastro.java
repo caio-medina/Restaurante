aca7ad362baf802a5af2b8086ace3d68c136bcf0
Caio Medina <caio__medina@hotmail.com>
2017-04-03 11:37:02 -0300

    Criacao das classes: Main - Cadastro - Cardapio - Conta - Pedido

---
+++ app/src/main/java/a2016_2/pooa/iff/br/restaurante/Cadastro.java
@@ -0,0 +1,13 @@
+package a2016_2.pooa.iff.br.restaurante;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Cadastro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro);
+    }
+}
