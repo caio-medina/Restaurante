d902b2b3b811054bae76651fe6a26649a286807c
Caio Medina <caio__medina@hotmail.com>
2017-04-03 12:29:55 -0300

    Fluxo de botoes funcionando

--- app/src/main/java/a2016_2/pooa/iff/br/restaurante/Cardapio.java
+++ app/src/main/java/a2016_2/pooa/iff/br/restaurante/Cardapio.java
@@ -1,7 +1,10 @@
 package a2016_2.pooa.iff.br.restaurante;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Cardapio extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Cardapio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cardapio);
+    }
+
+    public void chamaTelaPedido(View v) {
+        Toast.makeText(this, "Botão funcionando", Toast.LENGTH_SHORT);
+
+        Intent intent = new Intent(Cardapio.this, Pedido.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/a2016_2/pooa/iff/br/restaurante/Pedido.java
+++ app/src/main/java/a2016_2/pooa/iff/br/restaurante/Pedido.java
@@ -1,7 +1,10 @@
 package a2016_2.pooa.iff.br.restaurante;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Pedido extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class Pedido extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pedido);
+    }
+
+    public void chamaTelaConta(View v) {
+        Toast.makeText(this, "Botão funcionando", Toast.LENGTH_SHORT);
+
+        Intent intent = new Intent(Pedido.this, Conta.class);
+        startActivity(intent);
     }
 }
